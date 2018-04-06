a2acfd9f3859afc95e9789b99e18a054b4a5724e
Andru <roobotik@gmail.com>
2018-04-06 18:00:58 +0300

    fif child router in dashboard component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,20 @@ import { ModuleWithProviders } from '@angular/core';
 
 
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { CartCatalogComponent } from './dashboard/cart-catalog/cart-catalog.component';
 
 const routes: Routes = [
   { path: 'auth', loadChildren: './auth/auth.module#AuthModule'},
-  { path: 'user-page', loadChildren: './user/user-page.module#UserPageModule' },
   { path: 'not-found', loadChildren: './not-found/not-found.module#NotFoundModule' },
 
-  { path: '', component: DashboardComponent },
+  { path: '', component: DashboardComponent,
+  children: [
+    { path: '', component: CartCatalogComponent },
+    { path: 'user-page', loadChildren: './user/user-page.module#UserPageModule'  },
+
+
+  ]
+},
   // otherwise redirect to not-found
   { path: '**', redirectTo: 'not-found' }
 ];
